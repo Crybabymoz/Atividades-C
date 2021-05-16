1911cbe4ec3ae639c05bebf6d4b0fc641ecc2e05
Leila <78274564+Crybabymoz@users.noreply.github.com>
2021-05-15 21:45:49 -0300

    Add files via upload
    Exercícios simples de geometria :)

---
+++ exercícios/area do quadrado.c
@@ -0,0 +1,15 @@
+// Área do quadrado 
+#include <stdio.h>
+
+int main(void)
+{
+    int L;
+    int area;
+    
+    scanf("%d\n", & L);
+
+    area = L * L;
+    
+    printf("%d", area );
+    return 0;
+}
